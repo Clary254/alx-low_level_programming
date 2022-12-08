73a6ca2a54aee28f3e24b6d9ad1975b015fe89f0
Clary254 <clarencemugailwa@gmail.com>
2022-12-08 09:48:04 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-/*
- * main  -Print aline code using puts.
- *
- * Return : 0
- */
-int main (void)
-	puts("|"Programming is like building a multilingual puzzle");
-	return (0);
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
